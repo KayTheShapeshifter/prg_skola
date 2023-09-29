455b2d89b808aab3dd9f802a505a288488123403
unknown <tobias.halir@gmail.com>
2023-09-29 12:55:59 +0200

    idk funguje to

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -32,7 +32,7 @@ namespace Calculator
                         Environment.Exit(1);
                     }
                     else
-                    Console.WriteLine("Špatný input, zadej číslo znovu");
+                    Console.WriteLine("Špatný input, zadej operaci znovu");
                     operace = Console.ReadLine();
                 }
             }
@@ -61,34 +61,12 @@ namespace Calculator
                     }
                 default:
                     Console.WriteLine("Chyba: Neplatná operace.");
-                    return 0; // Return a default value if the operation is not recognized. 
+                    return 0; // to return 0 tady je, protoze jinak by to hodilo bug ze ne vzdy to da nejaky cislo, jinak je to useless protoze u operace to uz vyhodi vsechny pripady kde operace neni zadna z naprogramovanejch
             }
         }
 
         static void Main(string[] args)
         {
-            /*
-             * Pokud se budes chtit na neco zeptat a zrovna budu pomahat jinde, zkus se zeptat ChatGPT ;) - https://chat.openai.com/
-             * 
-             * ZADANI
-             * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
-             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
-             * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
-             * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
-             * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
-             * 5) Nadefinuj integerovou promennou result a prirad ji prozatimne hodnotu 0.
-             * 6) Vytvor podminky (if statement), podle kterych urcis, co se bude s cisly dit podle zadane operace
-             *    a proved danou operaci - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements.
-             * 7) Vypis promennou result do konzole
-             * 
-             * Mozna rozsireni programu pro rychliky / na doma (na poradi nezalezi):
-             * 1) Vypis do konzole pred nactenim kazdeho uzivatelova vstupu co po nem chces
-             * 2) a) Kontroluj, ze uzivatel do vstupu zadal, co mel (cisla, popr. ciselnou operaci). Pokud zadal neco jineho, napis mu, co ma priste zadat a program ukoncete.
-             * 2) b) To same, co a) ale misto ukonceni programu opakovane cti vstup, dokud uzivatel nezada to, co ma
-             *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
-             * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
-             */
-
             double num1;
             double num2;
             double result;
@@ -147,12 +125,11 @@ namespace Calculator
                         input2 = Console.ReadLine();
                     }
                 }
-                Console.WriteLine("Druhé číslo je " + input2 + ", nyní vyber operaci (soucet, rozdil, nasobeni, deleni)");
 
+                Console.WriteLine("Druhé číslo je " + input2 + ", nyní vyber operaci (soucet, rozdil, nasobeni, deleni)");
                 operace = ReadOperation(typ);
 
                 result = Calculate(num1, num2, operace, typ);
-
                 Console.WriteLine("Výsledek je " + result + ".");
 
                 Console.WriteLine("Přeješ si udělat nový výpočet? zadej \"ano\" pokuď chceš.");
