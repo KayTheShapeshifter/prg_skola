6c3e2ed486e54dd4efe5791435c2750400d897b2
unknown <tobias.halir@gmail.com>
2024-01-10 09:45:59 +0100

    game generation wt queue

--- Game1/Game1/Program.cs
+++ Game1/Game1/Program.cs
@@ -10,125 +10,150 @@ namespace Game1
 {
     internal class Program
     {
-        public static char[,] mapGeneration(char[,] map, int leftBarierIndex, int playableAreaSize)
+        public static char[] mapRow = new char[128]; //size of row
+        public static int playableAreaSize = 16; //size of the "road"
+        public static int InitleftBarierIndex = mapRow.Length / 2 - playableAreaSize / 2;
+        public static int mapCapacity = 10;
+        public static (char[], int) mapGeneration(char[] mapRow, int leftBarierIndex, int playableAreaSize, Queue<char[]> map)
         {
             Random random = new Random();
-            for (int j = 0; j < map.GetLength(0); j++)
+            int tempIndex = 0;
+            int tempDirection = 0;
+            if (leftBarierIndex == 1)//it could go off the map to the left if this wasnt here (if the border is so far left that the next step could make it go off it, then its prohibited to go left)
             {
-                int tempIndex = 0;
-                int tempDirection = 0;
-
-                if (leftBarierIndex == 1)//it could go off the map to the left if this wasnt here (if the border is so far left that the next step could make it go off it, then its prohibited to go left)
+                tempDirection = random.Next(2, 4);
+            }
+            else if (leftBarierIndex == mapRow.Length - playableAreaSize - 2)
+            {
+                int tempPlaceholder = random.Next(1, 3);
+                if (tempPlaceholder == 1)
                 {
-                    tempDirection = random.Next(2, 4);
-                }
-                else if (leftBarierIndex == map.GetLength(1) - 2)
-                {
-                    int tempPlaceholder = random.Next(1, 3);
-                    if (tempPlaceholder == 1)
-                    {
-                        tempDirection = 1;
-                    }
-                    else
-                    {
-                        tempDirection = 3;
-                    }
+                    tempDirection = 1;
                 }
                 else
                 {
-                    tempDirection = random.Next(1,4);
-                }
-
-                switch (tempDirection)
-                {
-                    case 1: //left
-                        leftBarierIndex --;
-                        for (int i = 0; i < leftBarierIndex - 1; i++) //left grass generation
-                        {
-                            map[j, tempIndex] = '#';
-                            tempIndex++;
-                        }
-                        map[j, tempIndex] = '/'; //border gen.
-                        tempIndex++;
-                        for (int i = 0; i < playableAreaSize; i++) //road gen
-                        {
-                            map[j, tempIndex] = ' ';
-                            tempIndex++;
-                        }
-                        map[j, tempIndex] = '/'; //border gen
-                        tempIndex++;
-                        for (int i = 0; i < map.GetLength(1) - (leftBarierIndex + playableAreaSize + 1); i++) //right grass gen
-                        {
-                            map[j, tempIndex] = '#';
-                            tempIndex++;
-                        }
-                        break;
-                    case 2: //right
-                        leftBarierIndex++;
-                        for (int i = 0; i < leftBarierIndex - 1; i++) //left grass generation
-                        {
-                            map[j, tempIndex] = '#';
-                            tempIndex++;
-                        }
-                        map[j, tempIndex] = '\\'; //border gen.
-                        tempIndex++;
-                        for (int i = 0; i < playableAreaSize; i++) //road gen
-                        {
-                            map[j, tempIndex] = ' ';
-                            tempIndex++;
-                        }
-                        map[j, tempIndex] = '\\'; //border gen
-                        tempIndex++;
-                        for (int i = 0; i < map.GetLength(1) - (leftBarierIndex + playableAreaSize + 1); i++) //right grass gen
-                        {
-                            map[j, tempIndex] = '#';
-                            tempIndex++;
-                        }
-                        break;
-                    default://center
-                        for (int i = 0; i < leftBarierIndex - 1; i++) //left grass generation
-                        {
-                            map[j, tempIndex] = '#';
-                            tempIndex++;
-                        }
-                        map[j, tempIndex] = '│'; //border gen.
-                        tempIndex++;
-                        for (int i = 0; i < playableAreaSize; i++) //road gen
-                        {
-                            map[j, tempIndex] = ' ';
-                            tempIndex++;
-                        }
-                        map[j, tempIndex] = '│'; //border gen
-                        tempIndex++;
-                        for (int i = 0; i < map.GetLength(1) - (leftBarierIndex + playableAreaSize + 1); i++) //right grass gen
-                        {
-                            map[j, tempIndex] = '#';
-                            tempIndex++;
-                        }
-                        break;
+                    tempDirection = 3;
                 }
             }
-            return map;
-        }
-        public static void printMap(char[,] map)
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
+            else
             {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.Write(map[i, j]);
-                }
-                Console.WriteLine();
+                tempDirection = random.Next(1, 4);
             }
+
+            switch (tempDirection)
+            {
+                case 1: //left
+                    leftBarierIndex--;
+                    for (int i = 0; i < leftBarierIndex - 1; i++) //left grass generation
+                    {
+                        mapRow[tempIndex] = '#';
+                        tempIndex++;
+                    }
+                    mapRow[tempIndex] = '/'; //border gen.
+                    tempIndex++;
+                    for (int i = 0; i < playableAreaSize; i++) //road gen
+                    {
+                        mapRow[tempIndex] = ' ';
+                        tempIndex++;
+                    }
+                    mapRow[tempIndex] = '/'; //border gen
+                    tempIndex++;
+                    for (int i = 0; i < mapRow.Length - (leftBarierIndex + playableAreaSize + 1); i++) //right grass gen
+                    {
+                        mapRow[tempIndex] = '#';
+                        tempIndex++;
+                    }
+                    break;
+                case 2: //right
+                    leftBarierIndex++;
+                    for (int i = 0; i < leftBarierIndex - 1; i++) //left grass generation
+                    {
+                        mapRow[tempIndex] = '#';
+                        tempIndex++;
+                    }
+                    mapRow[tempIndex] = '\\'; //border gen.
+                    tempIndex++;
+                    for (int i = 0; i < playableAreaSize; i++) //road gen
+                    {
+                        mapRow[tempIndex] = ' ';
+                        tempIndex++;
+                    }
+                    mapRow[tempIndex] = '\\'; //border gen
+                    tempIndex++;
+                    for (int i = 0; i < mapRow.Length - (leftBarierIndex + playableAreaSize + 1); i++) //right grass gen
+                    {
+                        mapRow[tempIndex] = '#';
+                        tempIndex++;
+                    }
+                    break;
+                default://center
+                    for (int i = 0; i < leftBarierIndex - 1; i++) //left grass generation
+                    {
+                        mapRow[tempIndex] = '#';
+                        tempIndex++;
+                    }
+                    mapRow[tempIndex] = '│'; //border gen.
+                    tempIndex++;
+                    for (int i = 0; i < playableAreaSize; i++) //road gen
+                    {
+                        mapRow[tempIndex] = ' ';
+                        tempIndex++;
+                    }
+                    mapRow[tempIndex] = '│'; //border gen
+                    tempIndex++;
+                    for (int i = 0; i < mapRow.Length - (leftBarierIndex + playableAreaSize + 1); i++) //right grass gen
+                    {
+                        mapRow[tempIndex] = '#';
+                        tempIndex++;
+                    }
+                    break;
+            }
+            return (mapRow, leftBarierIndex);
+        }
+        public static void printMap(Queue<char[]> map)
+        {
+            /*bool tempMapEmpty;
+            if (map.Count == 0)
+            {
+                tempMapEmpty = true;
+            }
+            else
+            {
+                tempMapEmpty = false;
+            }
+            
+            while (tempMapEmpty == false)
+            {
+
+            }*/
+            char[] tempMapRow = map.Dequeue();
+            for (int i = 0; i < tempMapRow.Length; i++)
+            {
+                Console.Write(tempMapRow[i]);
+            }
+            Console.WriteLine();
+        }
+        public static bool collisionDetection()
+        {
+            bool collisionDetection = false;
+            return collisionDetection;
         }
         static void Main(string[] args)
         {
-            char[,] map = new char[1000, 128];
-            int playableAreaSize = 16; //size of the "road"
-            int InitleftBarierIndex = map.GetLength(1) / 2 - playableAreaSize / 2;
+            int playerPosition = mapRow.Length / 2;
+            Queue<char[]> map = new Queue<char[]>(mapCapacity);
+            bool varCollisionDetection = collisionDetection();
 
-            map = mapGeneration(map, InitleftBarierIndex, playableAreaSize);
-            printMap(map);
+            while (varCollisionDetection == false) //always true now
+            {
+                Queue<char[]> tempMap = map;
+                while (map.Count <= mapCapacity)
+                {
+                    (mapRow, InitleftBarierIndex) = mapGeneration(mapRow, InitleftBarierIndex, playableAreaSize, map);
+                    map.Enqueue(mapRow);
+                }
+                printMap(tempMap);
+            }
             Console.ReadKey();
         }
     }
